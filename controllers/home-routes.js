b21a6b0ba81b3ebbe94266c41b52a30bcf90fd22
Ethan Willers <willers.enw@gmail.com>
2023-02-10 20:02:32 -0800

    main.handlebars working header

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -5,4 +5,12 @@ router.get('/', async (req, res) => {
     res.render('homepage');
 });
 
+router.get('/login', async (req, res) => {
+    res.render('login');
+});
+
+router.get('/dashboard', async (req, res) => {
+    res.render('dashboard');
+});
+
 module.exports = router;
