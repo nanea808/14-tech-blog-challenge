d1027b7b488f8ad9b6355662b3059f126b2276c1
Ethan Willers <willers.enw@gmail.com>
2023-02-12 20:25:28 -0800

    updated login view and added controllers/api/user-routes.js

--- server.js
+++ server.js
@@ -1,12 +1,35 @@
+const path = require('path');
 const express = require("express");
+const session = require('express-session');
 const exphbs = require("express-handlebars");
-// import sequelize connection
-const sequelize = require("./config/connection");
+// Import Sequelize with session store
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
+// Import sequelize connection
+const sequelize = require("./config/connection");
 const routes = require("./controllers");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+// Set up connection between session and our sequelize database
+const sess = {
+  secret: 'Super secret secret',
+
+  cookie: {
+    maxAge: 60 * 60 * 1000,
+    httpOnly: true,
+    secure: false,
+    sameSite: 'strict'
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+}
+
+app.use(session(sess));
 
 const hbs = exphbs.create({});
 
@@ -16,6 +39,7 @@ app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
